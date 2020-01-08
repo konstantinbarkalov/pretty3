37814760641d4af5cabc4b397cd6e1d49b59fc61
Konstantin Barkalov <mail@barkalov.ru>
2020-01-08 19:34:33 +0700

    tests

--- test/pretty.test.ts
+++ test/pretty.test.ts
@@ -12,12 +12,25 @@ describe('pretty stringifing tree to string variable', () => {
     const output = pretty.stringifyTree(input);
     expect(output).to.be.equal(reference);
   });
-  it('multiline tree', () => {
+  it('multiline node', () => {
     const input = JSON.parse(fs.readFileSync(path.resolve(testcaseRootPath, 'input02.json'), 'utf8'));
-    const reference = fs.readFileSync(path.resolve(testcaseRootPath, 'reference01.txt'), 'utf8');
+    const reference = fs.readFileSync(path.resolve(testcaseRootPath, 'reference02.txt'), 'utf8');
     const output = pretty.stringifyTree(input);
     expect(output).to.be.equal(reference);
   });
+  it('array-based root', () => {
+    const input = JSON.parse(fs.readFileSync(path.resolve(testcaseRootPath, 'input03.json'), 'utf8'));
+    const reference = fs.readFileSync(path.resolve(testcaseRootPath, 'reference03.txt'), 'utf8');
+    const output = pretty.stringifyTree(input);
+    expect(output).to.be.equal(reference);
+  });
+  it('long lists', () => {
+    const input = JSON.parse(fs.readFileSync(path.resolve(testcaseRootPath, 'input04.json'), 'utf8'));
+    const reference = fs.readFileSync(path.resolve(testcaseRootPath, 'reference04.txt'), 'utf8');
+    const output = pretty.stringifyTree(input);
+    expect(output).to.be.equal(reference);
+  });
+
 });
 
 
