f2fddce8cd14b127e34f177aac3fb04a93538ad2
Konstantin Barkalov <mail@barkalov.ru>
2020-01-26 10:13:43 +0700

    relink dev and test inputs

--- src/dev/runme8.ts
+++ src/dev/runme8.ts
@@ -1,20 +1,10 @@
 import {prettyOld, pretty} from '../index';
-import * as fs from 'fs';
 import { AnsiRenderer } from '../text/renderer/implementation/ansi';
-import { HtmlRenderer } from '../text/renderer/implementation/html';
-import { PlainRenderer } from '../text/renderer/implementation/plain';
 //import tree from './input05';
 import tree from './example01';
 
 const ansiRenderer = new AnsiRenderer();
-const htmlRenderer = new HtmlRenderer();
-const plainRenderer = new PlainRenderer();
 const ansiOutput = prettyOld.stringifyTree(tree, {renderer: ansiRenderer});
-const htmlOutput = prettyOld.stringifyTree(tree, {renderer: htmlRenderer});
-const plainOutput = prettyOld.stringifyTree(tree, {renderer: plainRenderer});
-fs.writeFileSync('./src/dev/output/tree.ansi', ansiOutput, 'utf8');
-fs.writeFileSync('./src/dev/output/tree.html', htmlOutput, 'utf8');
-fs.writeFileSync('./src/dev/output/tree.txt', plainOutput, 'utf8');
 console.log(ansiOutput);
 console.log('-----');
 pretty.print(tree);
